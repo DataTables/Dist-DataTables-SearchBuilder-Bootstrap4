0eddfcb350eb6a2caedddfeae161a475a7d19f18
dtbuild <dtbuild@rigil>
2026-07-22 14:50:47 +0000

    b2567bef1a733d021110553c86991410013af8de Release 2.0.0 ready
    Sync to source repo @b2567bef1a733d021110553c86991410013af8de

--- js/searchBuilder.bootstrap4.min.mjs
+++ js/searchBuilder.bootstrap4.min.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder Bootstrap 4 styling 2.0.0-beta.1 for DataTables
+/*! SearchBuilder Bootstrap 4 styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs4";import SearchBuilder from"datatables.net-searchbuilder";Object.assign(DataTable.SearchBuilder.classes,{clearAll:"btn btn-light dtsb-clearAll"}),Object.assign(DataTable.Group.classes,{add:"btn btn-light dtsb-add",clearGroup:"btn btn-light dtsb-clearGroup",logic:"btn btn-light dtsb-logic",search:"btn btn-light dtsb-search"}),Object.assign(DataTable.Criteria.classes,{condition:"form-control dtsb-condition",data:"form-control dtsb-data",delete:"btn btn-light dtsb-delete",left:"btn btn-light dtsb-left",right:"btn btn-light dtsb-right",value:"form-control dtsb-value"});export default DataTable;
